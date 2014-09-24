4452dc1d65e71182c4a63114212d2052bee47f55
Rokibul Hasan <rokibul@tasawr.com>
2014-09-24 16:55:01 +0600

    Define ability of operator and admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,9 @@ class Ability
     if user.admin?
       can :manage, :all
     elsif user.role?(:operator)
-      can :manage, [:school, :visit]
+      can :read, :all
+      cannot :manage, User
+      can [:read, :create, :update, :destroy], [School, Visit, Division, District, Thana, Union]
     end
   end
 end
