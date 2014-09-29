867f2ef8f12301505c00b7aabf57aa0ab53a9552
Rokibul Hasan <rokibul@tasawr.com>
2014-09-29 15:09:06 +0600

    Rename Projects

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
     elsif user.role?(:operator)
       can :read, :all
       cannot :manage, User
-      can [:read, :create, :update, :destroy], [School, Visit, Division, District, Thana, Union]
+      can [:read, :create, :update, :destroy], [School, Visit, Division, District, Thana]
     end
   end
 end
