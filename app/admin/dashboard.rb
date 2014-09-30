8dbeaa2b4a6b1b982e693e018cc1e0d6cc679d2e
rakib hasan <rakib063049@gmail.com>
2014-09-30 23:57:09 +0600

    Show additional data on index

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -6,7 +6,7 @@ ActiveAdmin.register_page "Dashboard" do
 
     columns do
       column do
-        panel "Approved Schools" do
+        panel "Recently Approved Schools" do
           table_for School.approved.order_as_district do
             column :year
             column :quarter
@@ -27,6 +27,57 @@ ActiveAdmin.register_page "Dashboard" do
             column("Total") { |school| school.total_students }
             column("Data Entry Operator") { |school| school.data_entry_operator }
             column("Data Entry Date") { |school| school.created_at }
+
+            column "Visit#1" do |school|
+              table_for school.first_visit do
+                column :visited_at
+                column :certificate do
+                  school.first_visit.acknowledgement_certificates.map { |img| link_to "Image", img.photo.url, target: '_blank' }.join(', ').html_safe
+                end
+                column :images do
+                  school.first_visit.images.map { |img| link_to "Image", img.photo.url, target: '_blank' }.join(', ').html_safe
+                end
+
+              end
+            end
+
+            column "Visit#2" do |school|
+              table_for school.second_visit do
+                column :visited_at
+                column :certificate do
+                  school.second_visit.acknowledgement_certificates.map { |img| link_to "Image", img.photo.url, target: '_blank' }.join(', ').html_safe
+                end
+                column :images do
+                  school.second_visit.images.map { |img| link_to "Image", img.photo.url, target: '_blank' }.join(', ').html_safe
+                end
+              end
+            end
+
+            column "Visit#3" do |school|
+              table_for school.third_visit do
+                column :visited_at
+                column :certificate do
+                  school.third_visit.acknowledgement_certificates.map { |img| link_to "Image", img.photo.url, target: '_blank' }.join(', ').html_safe
+                end
+                column :images do
+                  school.third_visit.images.map { |img| link_to "Image", img.photo.url, target: '_blank' }.join(', ').html_safe
+                end
+
+              end
+            end
+
+            column "Visit#4" do |school|
+              table_for school.fourth_visit do
+                column :visited_at
+                column :certificate do
+                  school.fourth_visit.acknowledgement_certificates.map { |img| link_to "Image", img.photo.url, target: '_blank' }.join(', ').html_safe
+                end
+                column :images do
+                  school.fourth_visit.images.map { |img| link_to "Image", img.photo.url, target: '_blank' }.join(', ').html_safe
+                end
+
+              end
+            end
           end
         end
       end
