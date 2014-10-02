59c4fb9aa7cbd2c4fa29beb5679cc9cc9d58b3b7
Rokibul Hasan <rokibul@tasawr.com>
2014-10-02 18:28:47 +0600

    Set up Puma, Nginx & Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,15 @@
+# Load DSL and Setup Up Stages
+require 'capistrano/setup'
+require 'capistrano/deploy'
+
+require 'capistrano/rails'
+require 'capistrano/bundler'
+require 'capistrano/rvm'
+require 'capistrano/puma'
+
+# Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
+Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
+
 # Change these
 server '203.76.108.68', port: 22, roles: [:web, :app, :db], primary: true
 
