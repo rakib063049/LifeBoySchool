270de5ae8ad7f20586a5f754f6976d54d1bffe98
Rokibul Hasan <rokibul@tasawr.com>
2014-10-02 18:38:43 +0600

    Set up Puma, Nginx & Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,39 +8,39 @@ require 'capistrano/rvm'
 require 'capistrano/puma'
 
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
-Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
+#Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
 
 # Change these
 server '203.76.108.68', port: 22, roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'git@github.com:rakib063049/LifeBoySchool.git'
-set :application,     'lifebuoy_school'
-set :user,            'dev'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :repo_url, 'git@github.com:rakib063049/LifeBoySchool.git'
+set :application, 'lifebuoy_school'
+set :user, 'dev'
+set :puma_threads, [4, 16]
+set :puma_workers, 0
 
 # Don't change these unless you know what you're doing
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :pty, true
+set :use_sudo, false
+set :stage, :production
+set :deploy_via, :remote_cache
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_error_log, "#{release_path}/log/puma.access.log"
+set :ssh_options, {forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub)}
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to true if using ActiveRecord
+set :puma_init_active_record, true # Change to true if using ActiveRecord
 
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
+# Defaults:
+set :scm, :git
+set :branch, :master
+set :format, :pretty
+set :log_level, :debug
+set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
 # set :linked_files, %w{config/database.yml}
@@ -85,10 +85,10 @@ namespace :deploy do
     end
   end
 
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
+  before :starting, :check_revision
+  after :finishing, :compile_assets
+  after :finishing, :cleanup
+  after :finishing, :restart
 end
 
 # ps aux | grep puma    # Get puma pid
