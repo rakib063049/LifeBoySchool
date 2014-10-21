e12fbc27398713d7476ac485292961350e963421
rakib hasan <rakib063049@gmail.com>
2014-10-22 00:05:07 +0600

    Recent change requests

--- app/helpers/active_admin/views_helper.rb
+++ app/helpers/active_admin/views_helper.rb
@@ -10,4 +10,9 @@ module ActiveAdmin::ViewsHelper
     data.strftime("%b %d, %Y | %I:%M %p")
   end
 
+  def custom_date_view(data)
+    return "" if data.nil?
+    data.strftime("%Y-%m-%d")
+  end
+
 end
